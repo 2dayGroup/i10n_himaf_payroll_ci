5ea63c2fc822a517c66f3eec75dac8966653590c
AFFECHI Achoué Marc Hilarion <marc_affechi@hotmail.com>
2023-07-06 16:08:38 +0000

    split employee name to firstname and lastname

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2010-2014 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "COTE D'IVOIRE - PAIE",
+    "version": "16.0.1.0.1",
+    "author": "2DAY GROUP",
+    "maintainer": "2DAY GROUP",
+    "website": "2daygroup.net",
+    "license": "AGPL-3",
+    "category": "Human Resources",
+    "summary": "Paie pour la Cote d'Ivoire",
+    "depends": ["hr", "hr_payroll"],
+    "data": ["views/hr_view.xml", "views/base_config_view.xml"],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+}
